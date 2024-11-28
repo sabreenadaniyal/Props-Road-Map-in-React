2c5429d724c5a6d5890a40311f758fc7a32c8620
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-11-28 14:18:26 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import ParentComponents from "./Components/ParentComponents";
 export default function Home() {
   return (
     
-    <div className="grid items-center justify-center p-8 pb-20
-    gap-2 font-sans">
+    <div>
     
       <ParentComponents />
 
